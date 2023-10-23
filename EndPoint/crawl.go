7359f89f2c7be9a6b74cbf642185fed9c3a4bf70
FCJ6202 <iit2020218@iiita.ac.in>
2023-10-24 03:57:11 +0530

    Edit the index.html page and make separate retry.html page

--- EndPoint/crawl.go
+++ EndPoint/crawl.go
@@ -34,6 +34,7 @@ func CovertValidLink(url string) string {
 
 // It crawl the data from url using http.get API call and return response
 func crawler(url string, w http.ResponseWriter) {
+	log.Printf("Processing url : %s", url)
 	// Check if the URL has been crawled in the last 60 minutes.
 	value, ok := Pages[url]
 	if ok && !value.Crosstime(float64(oldPagetime)) {
@@ -48,7 +49,7 @@ func crawler(url string, w http.ResponseWriter) {
 	// If the page is not cached, crawl it in real-time.
 	res, err := http.Get(url)
 	if err != nil {
-		http.Error(w, "Crawl function Failed to fetch the page", http.StatusInternalServerError)
+		http.Error(w, "Crawl function Failed to fetch the page whose url is"+url, http.StatusInternalServerError)
 		return
 	}
 	defer res.Body.Close()
@@ -56,14 +57,13 @@ func crawler(url string, w http.ResponseWriter) {
 	// Read the data from the io.ReadCloser.
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("error: %s", err)
 		return
 	}
 
 	// You can store the content on the disk or cache it for future use here.
 	Pages[url] = model.NewPage(url, string(data))
 
-	// fmt.Println(content)
 	// Return the content to the user.
 	fmt.Fprintf(w, "<p>%s</p>", Pages[url].Content.Data)
 
@@ -72,8 +72,6 @@ func crawler(url string, w http.ResponseWriter) {
 	*/
 	timeout := time.After(1 * 60 * time.Second)
 	<-timeout
-
-	fmt.Println(url)
 }
 
 // It call when user give post request in /crawl location
@@ -90,21 +88,23 @@ func CrawlHandler(w http.ResponseWriter, r *http.Request) {
 		URL := r.FormValue("url")
 		payingStatus := r.FormValue("pay") // 1 -> pay ,0 -> nonpay
 
-		if payingStatus == "1" {
+		if payingStatus == "paid" {
 			select {
 			case payedWorker <- struct{}{}:
 				crawler(CovertValidLink(URL), w)
+				log.Printf("Successfully Crawled url : %s", URL)
 				<-payedWorker
 			default:
-				retry.RetryPage(w, URL)
+				retry.RetryPage(w, URL, payingStatus)
 			}
 		} else {
 			select {
 			case nonpayedWorker <- struct{}{}:
 				crawler(CovertValidLink(URL), w)
+				log.Printf("Successfully Crawled url : %s", URL)
 				<-nonpayedWorker
 			default:
-				retry.RetryPage(w, URL)
+				retry.RetryPage(w, URL, payingStatus)
 			}
 		}
 	} else {
--- Retry/retry.go
+++ Retry/retry.go
@@ -1,25 +1,30 @@
 package retry
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func RetryPage(w http.ResponseWriter, URL string) {
-	// Display a retry page with a form to enter the URL again
-	fmt.Fprint(w, `
-		<html>
-		<head>
-			<title>Retry Page</title>
-		</head>
-		<body>
-			<h1>Retry</h1>
-			<p>There was an issue processing your request. Please try again.</p>
-			<form method="post">
-				<input type="text" name="url" value="`+URL+`">
-				<input type="submit" value="Retry">
-			</form>
-		</body>
-		</html>
-	`)
+func RetryPage(w http.ResponseWriter, URL, payingStatus string) {
+	tmpl, err := template.ParseFiles("Retry/retry.html")
+	if err != nil {
+		http.Error(w, "Failed to load retry page", http.StatusInternalServerError)
+		log.Printf("Error : %s", err)
+		return
+	}
+
+	data := struct {
+		URL          string
+		PayingStatus string
+	}{
+		URL:          URL,
+		PayingStatus: payingStatus,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Failed to render retry page", http.StatusInternalServerError)
+		log.Printf("Error : %s", err)
+	}
 }
